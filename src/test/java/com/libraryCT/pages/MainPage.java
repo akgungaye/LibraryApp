93d26a2525748dd610f6c5417c00a08345bde997
Mehmet <mhmtsytrk@gmail.com>
2020-11-07 22:43:55 +0300

    Library App starts

---
+++ src/test/java/com/libraryCT/pages/MainPage.java
@@ -0,0 +1,15 @@
+package com.libraryCT.pages;
+
+import com.libraryCT.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class MainPage extends BasePage {
+    public MainPage(){
+        PageFactory.initElements(Driver.get(),this);
+    }
+
+    // This is mehmet_@Us6
+
+}
